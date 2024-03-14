10f101301fb0b68a2d7a2bae2ca863a7ba0cd76d
Jakin Rogel <96803752+JakinRogel@users.noreply.github.com>
2024-03-14 16:37:28 -0700

    Update app.js

--- Webula/app.js
+++ Webula/app.js
@@ -1,25 +1,17 @@
-// Import the 'dotenv' module to load environment variables from a '.env' file
-require("dotenv").config();
-
-// Import the 'axios' module for making HTTP requests
-const axios = require("axios");
-
 // Access the API key from the environment variable
 const apiKey = process.env.API_KEY;
 
-// API endpoint
-const apiUrl = "https://api.example.com/data";
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    authorization: 'Bearer ${apiKey}'
+  }
+};
 
-// Make a GET request to the API endpoint with the API key
-axios
-  .get(apiUrl, {
-    headers: {
-      Authorization: `Bearer ${apiKey}`, // Using Bearer token authorization
-    },
-  })
-  .then((response) => {
-    console.log("API response:", response.data);
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+fetch('https://api.webflow.com/v2/sites', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+
